3e4fd52f7babaf751fb8ab5cd7923695093529ee
masudKhanGit <masud.git.developer@gmail.com>
2024-06-01 19:26:34 +0600

    routing study

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 1. Read and writing files fileSystem.js
 2. Create a simple web server server.js
+3. routing
--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const http = require('http');
+const url = require('url');
 const port = process.env | 8000;
 
 const server = http.createServer((req, res) => {
-    res.end('hello from the server');
+    // get path name
+    const pathName = req.url;
+
+    if(pathName === '/') {
+        res.end('This is home page');
+    } else if(pathName === '/overview') {
+        res.end('This is overview page');
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+
 });
 
 server.listen(port, () => {
