31320b191a9da65173e1bd016814f5ddb5a06141
masudKhanGit <masud.git.developer@gmail.com>
2024-06-01 21:38:46 +0600

    onk kicho poira laichi

--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 2. Create a simple web server server.js
 3. routing
 4. Build a simple api
+5. html templating build the templetes
--- server.js
+++ server.js
@@ -3,22 +3,46 @@ const url = require('url');
 const fs = require('fs');
 const port = process.env | 8000;
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+    output = temp.replace(/{%IMAGE%}/g, product.image)
+    output = temp.replace(/{%FORM%}/g, product.form)
+    output = temp.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    output = temp.replace(/{%QUANTITY%}/g, product.quantity)
+    output = temp.replace(/{%PRICE%}/g, product.price)
+    output = temp.replace(/{%ORGANIC%}/g, product.organic)
+    output = temp.replace(/{%DESCRIPTION%}/g, product.description)
+    output = temp.replace(/{%ID%}/g, product.id)
+    return output
+}
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-// const dataObj = JSON.parse(data);
-
+const dataObj = JSON.parse(data);
+const tempOverview = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
 
 const server = http.createServer((req, res) => {
     // get path name
     const pathName = req.url;
 
-    if(pathName === '/') {
-        res.end('This is home page');
-    } else if(pathName === '/overview') {
-        res.end('This is overview page');
+        // home page & overview page
+    if(pathName === '/' || pathName === '/overview') {
+        const cardHtml = dataObj.map(element => replaceTemplate(tempOverview, element));
+        console.log(cardHtml);
+
+        res.writeHead(200, { 'Content-type' : 'text/html' });
+        res.end(tempOverview);
+
+        // Product page
+    } else if(pathName === '/product') {
+        res.end('This is product page');
+
+        // API
     } else if(pathName === '/api') {
             res.writeHead(200, { 'Content-type' : 'application/json' });
             res.end(data);
+
+        //Not Found Page 
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
